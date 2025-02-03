afac6ca149a7e3a7bd369a6e45feb51c03cf7ede
scelo khoza <sceloprince749@gmail.com>
2025-02-03 20:42:34 +0200

    implementing logic to now get the weather as it is able to locate the user and returns their location

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 function fetchWeather(latitude, longitude) {
-    const apiKey = '';
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+    const apiKey = '8185dab8d28022fc9db915fb4a6fc6e3';
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        document.getElementById('weather').innerText = `Weather: ${data.weather[0].description}, ${data.main.temp}C`;
+        document.getElementById('Weather').innerText = `Weather: ${data.weather[0].description}, ${data.main.temp}C`;
     })
-    .catch(() =>{
-        document.getElementById('weather').innerText = "Weather info unavailable.";
+    .catch(() =>{ 
+        document.getElementById('Weather').innerText = "Weather info unavailable.";
     });
 }
 
 function fetchLocation() {
     if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position => {
-            const {
-                latitude, longitude
-            } = position.coords;
-            document.getElementById('location').innerText = `Lat: ${latitude}, Lon: ${longitude}`;
-            fetchWeather(latitude, longitude);
-        }, () => {
-            document.getElementById('location').innerText = "Location access denied.";
-        });
+        fetch('https://ipinfo.io/json?token=0eda6b5e4ee85f')
+        .then(response => response.json())
+        .then(data => {
+            const [latitude, longitude] = data.loc.split(',');
+            document.getElementById('location').innerText = `Country: ${data.country}, City: ${data.city}`;
+            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+            console.log(`Country: ${data.country}, City: ${data.city}`)
+            document.addEventListener('DOMContentLoaded', (event) => {
+                fetchWeather(latitude, longitude);
+            });
+            
+        })
+        .catch(error => console.error('Error fetching location:', error));
     } else {
         document.getElementById('location').innerText = "Geolocation not supported.";
     }
